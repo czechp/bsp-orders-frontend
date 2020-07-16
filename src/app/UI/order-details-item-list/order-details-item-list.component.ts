23d108a843b07d2f8e7d36b6122c08256f305515
P Czech <webcoderc@gmail.com>
2020-07-16 18:18:23 +0200

    Component to display info about items in order

--- src/app/UI/order-details-item-list/order-details-item-list.component.ts
+++ src/app/UI/order-details-item-list/order-details-item-list.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ItemInOrder } from 'src/app/Model/ItemInOrder';
 
 @Component({
   selector: 'app-order-details-item-list',
   templateUrl: './order-details-item-list.component.html',
   styleUrls: ['./order-details-item-list.component.css']
 })
-export class OrderDetailsItemListComponent implements OnInit {
+export class OrderDetailsItemListComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input()
+  public itemsInOrderList: ItemInOrder[];
+
+  constructor() {
+    this.itemsInOrderList=[];
+   }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.itemsInOrderList);
+  }
 
   ngOnInit(): void {
   }
