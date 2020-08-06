e99cb64c4cf21e13920a6ecca050a1505bceab87
PCzech <webcoderc@gmail.com>
2020-08-06 23:21:31 +0200

    Fetching superuser orders from api

--- src/app/ModelCRUD/order-superuser/order-superuser.component.ts
+++ src/app/ModelCRUD/order-superuser/order-superuser.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/Model/Order';
+import { HttpApiService } from 'src/app/Service/Http/http-api.service';
+import { orderEndpoint, superuserOrderStatusEndpoint } from 'src/app/Service/Http/URL';
 
 @Component({
   selector: 'app-order-superuser',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderSuperuserComponent implements OnInit {
 
-  constructor() { }
+  public currentOrders: Order[] = [];
+  public finishedOrders: Order[] = [];
+  public statement: string ="";
+  constructor(private httpApiService: HttpApiService) { }
 
   ngOnInit(): void {
+    this.getCurrentOrders();
   }
+
+  private getCurrentOrders():void{
+    this.httpApiService.get(orderEndpoint + "/" +  superuserOrderStatusEndpoint + "REALISE")
+    .subscribe(
+      response => {this.currentOrders = response; },
+      error => {this.statement="Błąd!!! Nie udało się pobrać zamówień z serwera"}
+    );
+  }
+
+  private getFinishedOrders():void{
+    this.httpApiService.get(orderEndpoint + "/" +  superuserOrderStatusEndpoint + "FINISHED")
+    .subscribe(
+      response => {this.finishedOrders = response},
+      error => {this.statement="Błąd!!! Nie udało się pobrać zamówień z serwera";}
+    );
+
+  }
+
+  
 
 }
