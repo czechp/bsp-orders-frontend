c9188af4835876d46342cd8e31650048a66d60a8
PCzech <webcoderc@gmail.com>
2020-08-10 23:12:50 +0200

    Changing base RestAPI URL

--- src/app/Service/Http/URL.ts
+++ src/app/Service/Http/URL.ts
@@ -1,10 +1,10 @@
-export const URL = "https://orders-bispol.herokuapp.com/api/";
+export const URL = "http://localhost:8080/api/";
 export const producerEndpoint = "producer";
 export const providerEndpoint = "provider";
 export const categoryEndpoint = "itemCategory";
 export const itemEndpoint = "item";
 export const loginEndpoint = "login";
-export const orderEndpoint = "order";
+export const orderEndpoint = "order";   
 export const registerEndpoint = "register";
 export const itemInOrderEndpoint = "itemInOrder";
 export const superuserOrderStatusEndpoint = "superuser/status/";
